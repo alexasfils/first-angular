99add7445bc9bdb7b6e7cc936a03ff18d2394d14
alexasfils <alessandrofilatov1@gmail.com>
2024-10-06 22:19:44 +0200

    fatto esercitazioni con ngIf, ngFor eccetera.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'corso-angilar2';
 
-  onClick(e: any) {
-    console.log(e)
+  isVisible = true;
+
+  persone = [
+    {
+      nome: 'pippo',
+      cognome: 'Verdi',
+      eta: 25,
+      isOnline: true
+    },
+    {
+      nome: 'Mimmo',
+      cognome: 'Neri',
+      eta: 20,
+      isOnline: false
+    },
+    {
+      nome: 'Ciro',
+      cognome: 'Gialli',
+      eta: 35,
+      isOnline: true
+    },
+     {
+      nome: 'Leonardo',
+      cognome: 'Crini',
+      eta: 45,
+      isOnline: false
+    },
+      {
+      nome: 'Peppe',
+      cognome: 'Ciombo',
+      eta: 27,
+      isOnline: true
+    }
+  ]
+
+  numero = 3;
+  stringa = 'paperino';
+
+  onClick(event: Event) {
+    this.title= 'ho cliccato sull bottone'
   
   }
 
   onInput(event: Event) {
+    this.title = (<HTMLInputElement>event.target).value
     console.log((<HTMLInputElement>event.target).value)
   
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routes';
 import { ProvaComponent } from './prova/prova.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-
-
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -19,11 +20,13 @@ import {MatInputModule} from '@angular/material/input';
   imports: [
       BrowserModule,
       AppRoutingModule,
+      FormsModule,
       BrowserAnimationsModule,
       MatSliderModule,
       MatCardModule,
       MatButtonModule,
-      MatInputModule
+      MatInputModule,
+      
   ],
   providers: [],
   bootstrap: [AppComponent]
