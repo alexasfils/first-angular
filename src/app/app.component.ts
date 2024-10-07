feca74d7c8594c23b6b44dcf09d0cf9db056c3a1
alexasfils <alessandrofilatov1@gmail.com>
2024-10-07 17:10:23 +0200

    Portare i dati dal parent all figlio Portare i dati dall figlio al padre ngClass, ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,49 +11,38 @@ export class AppComponent {
   isVisible = true;
 
   persone = [
-    {
-      nome: 'pippo',
-      cognome: 'Verdi',
-      eta: 25,
-      isOnline: true
-    },
-    {
-      nome: 'Mimmo',
-      cognome: 'Neri',
-      eta: 20,
-      isOnline: false
-    },
-    {
-      nome: 'Ciro',
-      cognome: 'Gialli',
-      eta: 35,
-      isOnline: true
-    },
-     {
-      nome: 'Leonardo',
-      cognome: 'Crini',
-      eta: 45,
-      isOnline: false
-    },
-      {
-      nome: 'Peppe',
-      cognome: 'Ciombo',
-      eta: 27,
-      isOnline: true
-    }
+    {nome: 'pippo', cognome: 'Verdi',eta: 25,isOnline: true},
+    {nome: 'Mimmo',cognome: 'Neri',eta: 20,isOnline: false},
+    {nome: 'Ciro',cognome: 'Gialli',eta: 35,isOnline: true},
+    {nome: 'Leonardo',cognome: 'Crini',eta: 45,isOnline: false},
+    {nome: 'Peppe',cognome: 'Ciombo',eta: 27,isOnline: true}
   ]
+  color = "green";
+  color2 = "red";
 
   numero = 3;
   stringa = 'paperino';
 
-  onClick(event: Event) {
-    this.title= 'ho cliccato sull bottone'
-  
+  onClick() {
+    this.persone = [
+    {nome: 'Piero', cognome: 'Verdi',eta: 25,isOnline: true},
+    {nome: 'Alessandro',cognome: 'Neri',eta: 20,isOnline: false},
+    {nome: 'Michele',cognome: 'Gialli',eta: 35,isOnline: true},
+    {nome: 'Leo',cognome: 'Crini',eta: 45,isOnline: false},
+    {nome: 'Perino',cognome: 'Ciombo',eta: 27,isOnline: true}
+    ]
   }
 
-  onInput(event: Event) {
-    this.title = (<HTMLInputElement>event.target).value
-    console.log((<HTMLInputElement>event.target).value)
+  onRiceviDati(value: string){
+    console.log(value);
+}
+  // onClick(event: Event) {
+  //   this.title= 'ho cliccato sull bottone'
+  // }
+
+  // onInput(event: Event) {
+  //   this.title = (<HTMLInputElement>event.target).value
+  //   console.log((<HTMLInputElement>event.target).value)
   
-  }
+  // }
 }
--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 //Model dell componente la parte Logica dell coponente
 @Component({
   selector: 'app-prova',
@@ -11,35 +11,52 @@ export class ProvaComponent implements OnInit{
   // immagine1 = 'https://www.bluegables.co.nz/cache/images/catalogue/313_2-398x319.jpg';
   // immagine2 = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
 
-  animali = [
-    {
-      nome: 'roger',
-      razza: 'golden',
-      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
-            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-            bred for hunting.`,
-      immagine: 'https://material.angular.io/assets/img/examples/shiba2.jpg'
-    },
-    {
-      nome: 'Spirit',
-      razza: 'Horse',
-      descrizione: `Spirit Horse is the biggest of the six original and distinct horces breeds of dog from Japan.
-            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-            bred for hunting.`,
-      immagine: 'https://www.bluegables.co.nz/cache/images/catalogue/313_2-398x319.jpg'
-    }
-  ]
+  // animali = [
+  //   {
+  //     nome: 'roger',
+  //     razza: 'golden',
+  //     descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+  //           A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+  //           bred for hunting.`,
+  //     immagine: 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+  //   },
+  //   {
+  //     nome: 'Spirit',
+  //     razza: 'Horse',
+  //     descrizione: `Spirit Horse is the biggest of the six original and distinct horces breeds of dog from Japan.
+  //           A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+  //           bred for hunting.`,
+  //     immagine: 'https://www.bluegables.co.nz/cache/images/catalogue/313_2-398x319.jpg'
+  //   }
+  // ]
+
+  //il Valore di (data) arriva da fuori
+  // @Input() data: any;
+  @Output() mandaDatiEvento = new EventEmitter<string>();
+
+  nome = 'Luca';
 
   constructor() {
     console.log("costruttore");
 }
-
+  
   ngOnInit(): void {
-    console.log("ngOnInit");
+    // console.log(this.data);
     // setInterval(() => {
     //   this.isDisabled = !this.isDisabled
     // }, 2000)
-    
   }
+  //mando a schermi i cambiamanti avvenuti dopo un qualsiasi evento. Per esempio adesso controllo cosa e' cambiato con il nostro arrey di dati
+// ngOnChanges(changes: SimpleChanges): void {
+//   console.log(changes);
+  //   }
+  
 
+  /*
+  prende i dati in questa situazione il nome dell prova.component.ts con una funzione manda dati gli passo il Output 
+  (mandaDatiEvento che prende il nome come paramentro e al click dell bottone su prova.component.html manda i dati presi dall prova.component.ts all app.component)
+  */
+  mandaDati() {
+    this.mandaDatiEvento.emit(this.nome);
+  }
 }
